f33ff3e93254e7a539d30b08d8111238864ee166
Maximilian Fettinger <maxi.fettinger@gmail.com>
2019-09-12 16:17:31 +0200

    hallo

---
+++ _Fettinger/test/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+      <b-container class="bv-example-row">
+          <b-row>
+              <b-col>1 of 3</b-col>
+              <b-col>2 of 3</b-col>
+              <b-col>3 of 3</b-col>
+          </b-row>
+      </b-container>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+
+</style>
