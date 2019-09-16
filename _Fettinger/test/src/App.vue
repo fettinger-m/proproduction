99358a920b593ac66ebe4b4a5e1ff52c05f10418
Maximilian Fettinger <maxi.fettinger@gmail.com>
2019-09-16 07:44:20 +0200

    commit

--- _Fettinger/test/src/App.vue
+++ _Fettinger/test/src/App.vue
@@ -1,24 +1,32 @@
 <template>
-  <div id="app">
-      <b-container class="bv-example-row">
-          <b-row>
-              <b-col>1 of 3</b-col>
-              <b-col>2 of 3</b-col>
-              <b-col>3 of 3</b-col>
-          </b-row>
-      </b-container>
-  </div>
+    <div id="app">
+        <b-container class="bv-example-row">
+            <b-row>
+                <b-col lg="2">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt
+                    ut labore et dolore magna aliquyam
+                </b-col>
+                <b-col lg="6">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt
+                    ut labore et dolore magna aliquyam
+                </b-col>
+                <b-col lg="2">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt
+                    ut labore et dolore magna aliquyam
+                </b-col>
+            </b-row>
+        </b-container>
+
+
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'app',
+        components: {
+            HelloWorld
+        }
+    }
 </script>
 
 <style>
