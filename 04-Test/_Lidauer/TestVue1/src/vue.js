0929f23ff4851ce7b8f9e3a1a7737590cd450457
Manuel Lidauer <manuel.lidauer@gmail.com>
2019-10-14 07:48:26 +0200

    Git Puschhh

---
+++ 04-Test/_Lidauer/TestVue1/src/vue.js
@@ -0,0 +1,35 @@
+
+
+
+
+/*
+
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        product: 'Milkyway Picture',
+        image: './assets/trecime.jpg',
+        inventory: 100,
+        onsale: true
+    }
+});
+
+
+
+var app2 = new Vue( {
+    el: '#app2',
+    data: {
+        todos: [
+            {text: 'uno'},
+            {text: 'due'},
+            {text: 'tre'}
+        ]
+    }
+});
+
+
+//Add an Element to the to do list
+app.todos.push({text: 'new item'});
+
+ */
