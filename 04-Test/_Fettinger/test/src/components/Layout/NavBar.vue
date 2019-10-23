b34e548086963ba01fd3cdf0a57fbbfadaf4555f
Maximilian Fettinger <maxi.fettinger@gmail.com>
2019-10-23 08:19:05 +0200

    1

--- 04-Test/_Fettinger/test/src/components/Layout/NavBar.vue
+++ 04-Test/_Fettinger/test/src/components/Layout/NavBar.vue
@@ -3,36 +3,37 @@
         <b-navbar-brand href="#">
             PRODUCTION
         </b-navbar-brand>
-        <b-navbar-toggle  target="nav-collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <b-navbar-toggle target="nav-collapse" data-target="#navbarSupportedContent"
+                         aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         </b-navbar-toggle>
         <b-collapse id="nav-collapse" is-nav>
             <b-navbar-nav class="mr-auto">
                 <b-nav-item href="#" active>
-                        <font-awesome-icon :icon="['fas', 'layer-group']" />
-                        Project
+                    <font-awesome-icon :icon="['fas', 'layer-group']"/>
+                    Project
                 </b-nav-item>
                 <b-nav-item href="#">
-                        <font-awesome-icon :icon="['fas', 'file-alt']" />
-                        Documents
+                    <font-awesome-icon :icon="['fas', 'file-alt']"/>
+                    Documents
                 </b-nav-item>
                 <b-nav-item href="#">
-                    <font-awesome-icon :icon="['fas', 'clipboard-list']" />
+                    <font-awesome-icon :icon="['fas', 'clipboard-list']"/>
                     Shotlist
                 </b-nav-item>
                 <b-nav-item href="#">
-                    <font-awesome-icon :icon="['fas', 'image']" />
+                    <font-awesome-icon :icon="['fas', 'image']"/>
                     Moodboard
                 </b-nav-item>
                 <b-nav-item href="#">
-                    <font-awesome-icon :icon="['fas', 'address-book']" />
+                    <font-awesome-icon :icon="['fas', 'address-book']"/>
                     Contacts
                 </b-nav-item>
                 <b-nav-item href="#">
-                    <font-awesome-icon :icon="['fas', 'map-marker-alt']" />
+                    <font-awesome-icon :icon="['fas', 'map-marker-alt']"/>
                     Location
                 </b-nav-item>
                 <b-nav-item href="#">
-                    <font-awesome-icon :icon="['fas', 'paperclip']" />
+                    <font-awesome-icon :icon="['fas', 'paperclip']"/>
                     Media
                 </b-nav-item>
             </b-navbar-nav>
