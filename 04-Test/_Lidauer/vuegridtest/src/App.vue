0581b41885f923c63d3503e9b4b5bc276c1538b2
Manuel Lidauer <manuel.lidauer@gmail.com>
2019-11-11 20:13:19 +0100

    DataTable+Calendar getestet

---
+++ 04-Test/_Lidauer/vuegridtest/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <DataTableTest/>
+    <Calendar></Calendar>
+  </div>
+</template>
+
+<script>
+import DataTableTest from './components/DataTableTest.vue'
+import Calendar from "@/components/Calendar";
+
+export default {
+  name: 'app',
+  components: {
+    Calendar,
+    DataTableTest
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ 04-Test/_Lidauer/vuegridtest/src/components/Calendar.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <h1>Calendar</h1>
+        <v-app id="inspire">
+            <v-sheet height="500">
+                <v-calendar
+                        type="month"
+                        now="2019-01-08"
+                        value="2019-01-08"
+                        :events="events"
+                ></v-calendar>
+            </v-sheet>
+        </v-app>
+    </div>
+</template>
+
+<script>
+    import events from "../events.json";
+
+    export default {
+        name: "Calendar",
+        data(){
+            return {
+                events_data: [],
+            }
+
+        },
+        mounted(){
+            this.events_data=events;
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
