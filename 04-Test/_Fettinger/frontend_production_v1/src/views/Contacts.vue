032121a3bfa60f375e1e10d30bfe4d589c6219c6
Manuel Lidauer <manuel.lidauer@gmail.com>
2019-12-06 11:43:22 +0100

    fetti test projekt

---
+++ 04-Test/_Fettinger/frontend_production_v1/src/views/Contacts.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="contacts">
+    <NavBar current-page="contacts"></NavBar>
+    <FilePath></FilePath>
+    <ProjectNameTitle></ProjectNameTitle>
+    <h2>Contacts</h2>
+  </div>
+</template>
+
+<script>
+
+import NavBar from "../components/Layout/NavBar";
+import FilePath from "../components/Layout/FilePath";
+import ProjectNameTitle from "../components/Layout/ProjectNameTitle";
+
+export default {
+  name: 'contacts',
+  components: {
+    NavBar,
+    FilePath,
+    ProjectNameTitle
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ 04-Test/_Lidauer/0-GESAMT/frontend_production_v1/src/views/Locations.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="location">
+    <NavBar current-page="location"></NavBar>
+    <FilePath></FilePath>
+    <ProjectNameTitle></ProjectNameTitle>
+    <h2>Locations</h2>
+  </div>
+</template>
+
+<script>
+
+import NavBar from "../components/Layout/NavBar";
+import FilePath from "../components/Layout/FilePath";
+import ProjectNameTitle from "../components/Layout/ProjectNameTitle";
+
+export default {
+  name: 'location',
+  components: {
+    NavBar,
+    FilePath,
+    ProjectNameTitle
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
